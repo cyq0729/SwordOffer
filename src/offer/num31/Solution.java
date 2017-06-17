fe9d95997b388a8f27f90bedaad7563c083f0421
石琦峰 <byhieg@gmail.com>
2017-06-17 22:21:06 +0800

    添加第31题

---
+++ src/offer/num31/Solution.java
@@ -0,0 +1,29 @@
+package offer.num31;
+
+/**
+ * Created by byhieg on 17/6/17.
+ * Mail to byhieg@gmail.com
+ */
+public class Solution {
+
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if(array.length == 0 || array == null){
+            return 0;
+        }
+        int currentSum = 0;
+        int biggestSum = Integer.MIN_VALUE;
+        for(int i = 0 ; i < array.length;i++){
+            if(currentSum <= 0){
+                currentSum = array[i];
+            }else{
+                currentSum += array[i];
+            }
+
+            if(currentSum > biggestSum){
+                biggestSum = currentSum;
+            }
+        }
+        return biggestSum;
+
+    }
+}
